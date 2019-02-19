028e82886cd57e5d55882221aa067c9876d1951b
Fabrício Rocha <fabricio.lopesx@gmail.com>
2019-02-19 23:34:36 +0000

    Wip layout - add message erro

--- platforms/browser/www/js/globalFunctions.js
+++ platforms/browser/www/js/globalFunctions.js
@@ -11,6 +11,8 @@ function addItem(){
     
         createArray();
     }
+
+    $('.wrap-error').hide(200);
 }
 
 function createArray(){
@@ -32,26 +34,31 @@ function criaLista(){
     $('.js-received-items li').each(function(i, elem){
         listReady.push($(elem).text());
     })
+
+    if(listReady.length > 0){
+        var jsonString = JSON.stringify(listReady);
+        $.ajax({
     
-    var jsonString = JSON.stringify(listReady);
-    $.ajax({
-
-        type: "POST",
-        //url:'http://localhost:8080/servidor/inserts.php', //WORK
-        //url:'http://localhost/servidor2/inserts.php', //HOME
-        url:'http://uxdeveloper.online/servidor2/inserts.php', //ONLINE
-        data: {data : jsonString, nameList},
-        success: function(){
-            alert("OK!!!");
-
-            $('.js-received-items li').remove();
-            printList();
-            window.location.href = "index.html#my-lists";
-        },
-        error: function(){
-            alert("Erro no envio ajax");
-        }
-    });
+            type: "POST",
+            //url:'http://localhost:8080/servidor/inserts.php', //WORK
+            //url:'http://localhost/servidor2/inserts.php', //HOME
+            url:'http://uxdeveloper.online/servidor2/inserts.php', //ONLINE
+            data: {data : jsonString, nameList},
+            success: function(){
+                alert("OK!!!");
+    
+                $('.js-received-items li').remove();
+                printList();
+                window.location.href = "index.html#my-lists";
+            },
+            error: function(){
+                alert("Erro no envio ajax");
+            }
+        });
+    } else{
+        $('.wrap-error').show(200);
+    }
+    
 }
 
 function printList(){
